56d7e996209a0c35262ed465c19a96b94e6fbd76
Heater108 <michael.virgato@gmail.com>
2019-02-11 18:11:06 +1100

    g

--- Bertoni Code/bertoni_numerical.py
+++ Bertoni Code/bertoni_numerical.py
@@ -38,8 +38,25 @@ def thermalization_time(DM_mass):
         therm_time_array = np.append(therm_time_array, 1/scattering_rate(DM_mass, energy_array[i]))
         new_energy = energy_averge(energy_array[i])
         energy_array = np.append(energy_array, new_energy)
-        i += 1 
-    
-    return np.sum(therm_time_array)
+        i += 1
 
-print(thermalization_time(1)*time_conversion/3.154e7)
+    return np.sum(therm_time_array)*time_conversion/3.154e7
+
+
+def make_plot_time():
+    mass_range = np.logspace(-6, 1, num = 100)
+    therm_array = np.empty(0)
+
+    for i in mass_range:
+        dummy = thermalization_time(i)
+        therm_array = np.append(therm_array, dummy)
+
+    fig, ax = plt.subplots(figsize = (10, 7), dpi = 100)
+    ax.plot(mass_range, therm_array)
+    ax.set_xscale('log')
+    #ax.axis([1e-6, 1e1, 1e-62, 1e-51])
+    ax.set(xlabel = r'Mass of DM [GeV]', ylabel = r'Thermalization Time')
+    plt.savefig('Thermalzation time.png')
+    plt.show()
+
+make_plot_time()
