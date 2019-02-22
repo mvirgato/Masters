5ad0834eaa89f12a395d2ba13b153f700f131ed2
Michael <michael.virgato@gmail.com>
2019-02-22 11:14:49 +1100

    g

--- pseudoscalar.py
+++ pseudoscalar.py
@@ -1,1 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+SMALL_SIZE = 14
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 18
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+pi = np.pi
+
+Temp = (1e5 / (1.16e4) * 1e-9)  # Neutron star temperature in GeV
+neutron_mass = (0.939)  # mass of neutron in GeV
+therm_time = (1e10 * 3.154e7 / (6.58e-16) * 1e9)  # in GeV^-1
