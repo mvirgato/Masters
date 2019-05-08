66822382d2d9b0c60d823a1bd11d6a18e9387d71
Heater108 <michaelvirgato@gmail.com>
2019-05-07 22:49:30 -0700

    added cubeature

--- C_Code/caprate.c
+++ C_Code/caprate.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <gsl/gsl_sf_bessel.h>
+
+#include "cubature.h"
+
+#define VERBOSE 0
+
+#if defined(PCUBATURE)
+#  define cubature pcubature
+#else
+#  define cubature hcubature
+#endif
 
 //GLOBAL CONSTANTS
 #define ESCAPE_VEL sqrt((2 * 6.67408E-11 * 1.4 * 2E30) / (10E3))	//NS escape velocity in m/s. Need to make into function of radius
@@ -9,43 +18,42 @@
 #define NM 0.939	//neutron mass in GeV
 #define FERMI_ENERGY 0.085 // in GeV
 #define FERMI_VEL (2 * FERMI_ENERGY) / NM
-#define SOL 299792458 // speed of light in vacuum m/s
+#define SOL 299792458.0 // speed of light in vacuum m/s
 #define VELDISP 270e3 //DM velocity dispersion for MB dist
 #define NSVEL 200e3 // NS velocity in galactic frame
 
 //GENERAL FUNCTIONS
-float mu(float dm){
+double mu(double dm){
 	/* RATIO OF DARK MATTER TO NEUTRON MASS */
 
 	return dm / NM;
 }
 
-float mu_plus(float dm){
+double mu_plus(double dm){
 
-	return ( 1 + mu(dm)) / (float)2;
+	return ( 1 + mu(dm)) / (double)2;
 }
 
 //TRIG FUNCTIONS
-float initial_cos(float s, float t){
+double initial_cos(double s, double t){
 	/* angle between incoming DM and neutron */
-
-	float num = powf(ESCAPE_VEL, 2) - powf(s, 2) - powf(t, 2);
-	float den = 2 * s * t;
+    double num = ESCAPE_VEL * ESCAPE_VEL - s * s - t * t;
+	double den = 2 * s * t;
 
 	return num / den;
 }
 
-float final_cos(float s, float t, float v){
+double final_cos(double s, double t, double v){
 	/* angle between outgoing DM and neutron */
 
-	float num = powf(v, 2) - powf(s, 2) - powf(t, 2);
-	float den = 2 * s * t;
+	double num = v * v - s * s - t * t;
+	double den = 2 * s * t;
 
 	return num / den;
 }
 
 //STEP FUNCTIONS
-float step(float f){
+double step(double f){
 	/* a step function to constrain integration region */
 	if ( f > 1 || f < -1){
 		return 0;
@@ -55,40 +63,77 @@ float step(float f){
 	}
 }
 
-int heaviside_product(float s, float t, float v){
+int heaviside_product(double s, double t, double v){
 	/* product of relevant step functions */
-	return step(initial_cos(s, t)) * final_cos(s, t, v);
+	return step(initial_cos(s, t)) * step(final_cos(s, t, v));
 }
 
 //ENERGIES
-float initial_vel(float s, float t, float dm){
-	/* centre of mass value of initial neutron velocity */
-	float a = 2 * mu(dm) * mu_plus(dm) * powf(t, 2) + 2 * mu_plus(dm) * powf(s, 2) - mu(dm) * powf(ESCAPE_VEL, 2);
-	return a / powf(SOL, 2);
-}
 
-float final_vel(float s, float t, float v, float dm){
+
+double velsqr(double s, double t, double v, double dm){
 	/*centre of mass value of final neutron velocity */
-	float a = 2 * mu(dm) * mu_plus(dm) * powf(t, 2) + 2 * mu_plus(dm) * powf(s, 2) - mu(dm) * powf(v, 2);
-	return a / powf(SOL, 1);
+	double a = 2 * mu(dm) * mu_plus(dm) * t * t + 2 * mu_plus(dm) * s*s- mu(dm) * v * v;
+	return a / (SOL*SOL);
 }
 
-float initial_energy(float s, float t, float dm){
-	return 0.5 * NM * initial_vel(s, t, dm);
+double energy(double s, double t, double v, double dm){
+	return 0.5 * NM * velsqr(s, t, v, dm);
 }
 
-float final_energy(float s, float t, float v, float dm){
-	return 0.5 * NM * final_vel(s, t, v, dm);
-}
+
 //FERMI-DIRAC DISTRIBUTION FUNCTIONS
 
-float initial_FD(float s, float t, float dm){
-	return 1 / (float)( 1 + exp( ( initial_energy(s, t, dm) - FERMI_ENERGY ) / TEMP ) );
+double FD(double s, double t, double v, double dm){
+	return 1 / (double)( 1 + exp( ( energy(s, t, v , dm) - FERMI_ENERGY ) / TEMP ) );
+}
+
+struct int_params {double dm;};
+
+int myintegrand(unsigned ndim, const double *x, void *p, unsigned fdim, double *fval){
+
+    struct int_params *params = (struct int_params *)p;
+    
+    double dm = (params->dm);
+    
+    fval[0] = heaviside_product(x[1], x[2], x[0]) * FD(x[1], x[2], ESCAPE_VEL, dm) * (1 - FD(x[1], x[2], x[0], dm));
+    
+    return 0;
+}
+
+double tbound(double dm){
+    return 1.05 * sqrt( (SOL*SOL * FERMI_VEL + mu(dm) * ESCAPE_VEL*ESCAPE_VEL) / (2 * mu(dm) * mu_plus(dm) ));
+}
+
+double sbound(double dm){
+    return 1.05 * sqrt( (SOL*SOL * FERMI_VEL + mu(dm) * ESCAPE_VEL*ESCAPE_VEL) / (2 * mu(dm)));
+}
+
+
+
+double doing_integral(double dm){
+    
+    double val, error;
+    
+    struct int_params params = {dm};
+    
+    double smax = sbound(dm);
+    double tmax = tbound(dm);
+    double vmax = ESCAPE_VEL;
+    
+    double xl[3] = {0, 0, 0};
+    double xu[3] = {vmax, smax, tmax};
+    
+    hcubature(1, &myintegrand, &params, 3, xl, xu, 0, 1e-6, 1e-6, ERROR_INDIVIDUAL, &val, &error);
+    
+    return val;
+    
+
 }
 
 //MAIN
 int main(){
-	float a = initial_FD(1, 1, 1);
-	printf("%f\n", a );
+    double a = doing_integral(1e-2);
+    printf("%.10e\n", a);
 	return 0;
 }
