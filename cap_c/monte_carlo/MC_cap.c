e52d23606ba41fdec6b4add4c45a7fdea15ae782
Michael Virgato <mvirgato@student.unimelb.edu.au@6400d-117818-l.mobility.unimelb.net.au>
2019-06-13 11:23:56 +1000

    ok

--- cap_c/monte_carlo/MC_cap.c
+++ cap_c/monte_carlo/MC_cap.c
@@ -41,21 +41,21 @@ double rate_integral(double dm, double radius, int npts){
 
   gsl_monte_function G = { &myintegrand, 3, &params }; // {function, dimension, params}
 
-  size_t calls = 500000;
+  size_t calls = 100000;
 
   gsl_rng_env_setup ();
 
   T = gsl_rng_default;
   r = gsl_rng_alloc (T);
 
-  // {
-  //   gsl_monte_plain_state *s = gsl_monte_plain_alloc (3);
-  //   gsl_monte_plain_integrate (&G, xl, xu, 3, calls, r, s,
-  //                              &res, &err);
-  //   gsl_monte_plain_free (s);
-  //
-  //   // display_results ("plain", res, err);
-  // }
+  {
+    gsl_monte_plain_state *s = gsl_monte_plain_alloc (3);
+    gsl_monte_plain_integrate (&G, xl, xu, 3, calls, r, s,
+                               &res, &err);
+    gsl_monte_plain_free (s);
+
+    // display_results ("plain", res, err);
+  }
 
   // {
   //   gsl_monte_miser_state *s = gsl_monte_miser_alloc (3);
@@ -66,28 +66,28 @@ double rate_integral(double dm, double radius, int npts){
   //   display_results ("miser", res, err);
   // }
   //
-  {
-    gsl_monte_vegas_state *s = gsl_monte_vegas_alloc (3);
-
-    gsl_monte_vegas_integrate (&G, xl, xu, 3, 100000, r, s,
-                               &res, &err);
-    // display_results ("vegas warm-up", res, err);
-
-    // printf ("converging...\n");
-
-    do
-      {
-        gsl_monte_vegas_integrate (&G, xl, xu, 3, calls/5, r, s,
-                                   &res, &err);
-        // printf ("result = % .6e sigma = % .6f "
-        //         "chisq/dof = %.1e\n", res, err, gsl_monte_vegas_chisq (s));
-      }
-    while (fabs (gsl_monte_vegas_chisq (s) - 1.0) > 0.5);
-
-    // display_results ("vegas final", res, err);
-
-    gsl_monte_vegas_free (s);
-  }
+  // {
+  //   gsl_monte_vegas_state *s = gsl_monte_vegas_alloc (3);
+  //
+  //   gsl_monte_vegas_integrate (&G, xl, xu, 3, 100000, r, s,
+  //                              &res, &err);
+  //   // display_results ("vegas warm-up", res, err);
+  //
+  //   // printf ("converging...\n");
+  //
+  //   do
+  //     {
+  //       gsl_monte_vegas_integrate (&G, xl, xu, 3, calls/5, r, s,
+  //                                  &res, &err);
+  //       // printf ("result = % .6e sigma = % .6f "
+  //       //         "chisq/dof = %.1e\n", res, err, gsl_monte_vegas_chisq (s));
+  //     }
+  //   while (fabs (gsl_monte_vegas_chisq (s) - 1.0) > 0.5);
+  //
+  //   // display_results ("vegas final", res, err);
+  //
+  //   gsl_monte_vegas_free (s);
+  // }
 
   gsl_rng_free (r);
 
@@ -114,7 +114,6 @@ double r_integrand(double x, void *p){
 
 double all_integrals(double dm, int npoints){
 
-    int NP = 100;
 
 //    gsl_integration_workspace * w = gsl_integration_workspace_alloc (NP);
 
@@ -130,9 +129,9 @@ double all_integrals(double dm, int npoints){
     F.function = &r_integrand;
     F.params = &params2; // {function, dimension, params}
 
-    size_t calls = 100;
+    size_t calls = 1000;
 
-    gsl_integration_qng (&F, 1. , 11. , 0.0 , 1e-7, &res, &err, &calls );
+    gsl_integration_qng (&F, 1. , 11. , 0.0 , 1e-2, &res, &err, &calls );
 
 
   //  gsl_integration_workspace_free (w);
@@ -198,24 +197,25 @@ int main ()
     npts = readdata("eos_24_lowmass.dat");
 
     double test = all_integrals(1, npts);
+
     printf("all integrals = %0.10E\n", test);
 
   //   int i;
-  //  
+  //
   //   int range = 20;
   //   double mass_vals[range];
-  //  
+  //
   //   logspace(-6, 1, range, mass_vals);
-  //  
-  //  
+  //
+  //
   //   FILE *outfile = fopen("cap_rate_MC.dat", "w");
-  //  
+  //
   //   for (i = 0; i < range; i++){
-  //  
+  //
   //   fprintf(outfile,"%0.10E\t%.10E\n", mass_vals[i], rate_integral( mass_vals[i], 11, npts ) );
-  //  
+  //
   //   }
-  //  
+  //
   //   fclose(outfile);
 
     return 0;
