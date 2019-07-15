5e04defa51ee73c422f45f09530ec031f13b1c2c
Heater108 <michael.virgato@gmail.com>
2019-07-14 22:47:27 -0700

    ok

--- cap_rate/analytic_cap/zero_temp/zero_temp.c
+++ cap_rate/analytic_cap/zero_temp/zero_temp.c
@@ -121,15 +121,34 @@ double SixMuI (double initvel, double finvel, double DMmass, double chempot){
 
 
 
-  double l1 = -(2.*FV + finvel*MU)*(FV - finvel*MU)*(FV - finvel*MU)*( step((RM - FV)/(2.*MUP)) + step( (FV - RP)/(2.*MUP) ) );
+  double l1  = (2.*FV + finvel*MU)*(FV - finvel*MU)*(FV - finvel*MU)*( step((RM - FV)/(2.*MUP)) + step( (FV - RP)/(2.*MUP) ) );
 
-  double l2 = -2.*( MU*( 2.*MU + 3.)*finvel*finvel - 6.*MU*MUP*finvel*finvel - FV*MU*finvel + 2.*FV*FV ) * (FV - finvel*MU)*step( (FV - PM)/(2.*MUP) )*step( (RP - FV)/(2.*MUP) );
+  double l2  = 2.*( MU*( 2.*MU + 3.)*finvel*finvel - 6.*MU*MUP*finvel*finvel - FV*MU*finvel + 2.*FV*FV ) * (FV - finvel*MU)*step( (FV - PM)/(2.*MUP) )*step( (RP - FV)/(2.*MUP) );
 
-  double l3 = -3.*MU*MUP*MUP*( (-finvel - initvel)*(finvel*finvel - initvel*initvel)*step( FV*FV - AM*AM ) );
+  double l3  = 3.*MU*MUP*MUP*( (-finvel - initvel)*(finvel*finvel - initvel*initvel)*step( FV*FV - AM*AM ) );
 
-  double l4 = -3.*MU*MUP*MUP*( -(finvel - initvel)*( (finvel*finvel - initvel*initvel)*( step( FV*FV - AP*AP ) - step( FV*FV - BP*BP) ) ) - (finvel + initvel)*(finvel + initvel)*step( FV*FV - BM*BM ) );
+  double l4  = 3.*MU*MUP*MUP*( -(finvel - initvel)*( (finvel*finvel - initvel*initvel)*( step( FV*FV - AP*AP ) - step( FV*FV - BP*BP) ) ) - (finvel + initvel)*(finvel + initvel)*step( FV*FV - BM*BM ) );
 
-  return l1 + l2 + l3 + l4;
+  double l5  = (FV - initvel*MU)*(FV - initvel*MU)*(2.*FV + initvel*MU)*step( (FV + AM)/(2.*MUP) )*step( (AP - FV)/(2.*MUP) );
+
+  double l6  = (2.*FV - initvel*MU)*(FV + initvel*MU)*(FV + initvel*MU)* step( (AM - FV)/(2.*MUP) )*step( (FV + AM)/(2.*MUP) );
+
+  double l7  = (FV + finvel*MU)*(MU*(2.*MU + 3.)*finvel*finvel - 6.*MU*MUP*finvel*finvel + FV*MU*finvel + 2.*FV*FV)*( step( (FV +RM)/(2.*MUP) ) +step(-(FV + RP)/(2.+MUP)) );
+
+  double l8  = (finvel*MU - LP)*(2.*MU*MU*finvel*finvel -6.*MU*MUP*finvel*finvel - MUP*LP*finvel + 3.*FV*FV - LP*LP +3.*initvel*initvel*MU)*( step( (RM - LP)/(2.*MUP) ) + step( -(LP - RP)/(2.*MUP) ) );
+
+  double l9  = (finvel*MU + LP)*(2.*MU*MU*finvel*finvel -6.*MU*MUP*finvel*finvel + MUP*LP*finvel + 3.*FV*FV - LP*LP +3.*initvel*initvel*MU)*( step( (RM + LP)/(2.*MUP) ) + step( -(LP + RP)/(2.*MUP) ) );
+
+  double l10 = (finvel*MU - LP)*( -6.*MU*MUP*finvel*finvel + 2.*FV*FV +MU*( (2.*MU +1)*finvel*finvel - LP*finvel +2.*initvel*initvel ) )*step( (LP - PM)/(2.*MUP) )*step( (RP - LP)/(2.*MUP) );
+
+  double l11 = (finvel*MU + LP)*( -6.*MU*MUP*finvel*finvel + 2.*FV*FV +MU*( (2.*MU +1)*finvel*finvel + LP*finvel +2.*initvel*initvel ) )*step( -(LP + PM)/(2.*MUP) )*step( (RP + LP)/(2.*MUP) );
+
+  double l12 = (initvel*MU - LM)*(2.*FV*FV + MU*( 2.*finvel*finvel + initvel*( 2.*MU*initvel + initvel - LM ) ) - 6.*initvel*initvel*MU*MUP)*step( (AM + LM)/(2.*MUP) )*step( (AP - LM)/(2.*MUP) );
+
+  double l13 = (initvel*MU + LM)*(2.*FV*FV + MU*( 2.*finvel*finvel + initvel*( 2.*MU*initvel + initvel + LM ) ) - 6.*initvel*initvel*MU*MUP)*step( (AM - LM)/(2.*MUP) )*step( (AP + LM)/(2.*MUP) );
+
+
+  return -l1 - l2 - l3 - l4 + l5 - l6 + l7 - l8 - l9 - l10 - l11 + l12 + l13;
 }
 
 int main()
