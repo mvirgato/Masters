63a5b56f652d29c1facd03070216a8d587b1a65c
Heater108 <michael.virgato@gmail.com>
2019-09-15 21:44:32 +1000

    ok

--- annihilation/plotter.py
+++ annihilation/plotter.py
@@ -1,45 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+names = []
 
+for i in range(11)[1:]:
+    names.append('d'+ str(i))
 
 def ann_plots():
 
-    lines = []
-    domain = np.empty(0)
-    range1 = np.empty(0)
-    range2 = np.empty(0)
-    range3 = np.empty(0)
-    range4 = np.empty(0)
-    range5 = np.empty(0)
-    range6 = np.empty(0)
+    for j in names:
+        lines = []
 
-    current_file = open('d8ACS.dat', 'r')
-    lines = current_file.readlines()
+        mydict = {}
 
-    for x in lines:
-        x = x.split('\t')
-        domain = np.append(domain, float(x[0]))
-        range1 = np.append(range1, float(x[1]))
-        range2 = np.append(range2, float(x[2]))
-        range3 = np.append(range3, float(x[3]))
-        range4 = np.append(range4, float(x[4]))
-        range5 = np.append(range5, float(x[5]))
-        range6 = np.append(range6, float(x[6]))
+        current_file = open(j + 'ACS.dat', 'r')
+        lines = current_file.readlines()
 
-    current_file.close
+        for i in range(len(lines[0].split('\t'))):
+            mydict['range'+ str(i)] = np.empty(0)
 
-    fig, ax1 = plt.subplots()
-    ax1.loglog(domain, range1)
-    ax1.loglog(domain, range2)
-    ax1.loglog(domain, range3)
-    ax1.loglog(domain, range4)
-    ax1.loglog(domain, range5)
-    ax1.loglog(domain, range6)
-    ax1.set(xlabel = r'$m_\chi$ [GeV]', ylabel = r'$G^2$ $[GeV^{-2}]$')
-    # ax1.set_yscale('log')
-    # ax1.axis([0, 12, 0, 1.2e56])
-    plt.savefig('d8ACS.pdf')
+        for x in lines:
+            x = x.split('\t')
 
+            for i in range(len(x)):
+                mydict['range'+ str(i)] = np.append(mydict['range'+ str(i)], float(x[i]))
+
+        fig, ax1 = plt.subplots()
+        for i in range(len(lines[0].split('\t')))[1:]:
+            ax1.loglog(mydict['range0'], mydict['range'+ str(i)])
+        ax1.set(xlabel = r'$m_\chi$ [GeV]', ylabel = r'$G^2$ $[GeV^{-2}]$')
+        plt.savefig(j + 'ACS.pdf')
+
+    
 
 ann_plots()
