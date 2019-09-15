b75a29957902e6dbe74e6b826647f13e0bef2618
mvirgato <michael.virgato@gmail.com>
2019-09-15 18:33:14 +1000

    ok

--- annihilation/plotter.py
+++ annihilation/plotter.py
@@ -3,76 +3,43 @@ import matplotlib.pyplot as plt
 
 
 
-files = ['misner_full_cap', 'misner_no_vel_cap', 'vegas_no_vel_cap', 'vegas_full']
+def ann_plots():
 
-def cap_rate_plots():
-    """
-    Plots capture rate
-    """
     lines = []
     domain = np.empty(0)
-    range = np.empty(0)
+    range1 = np.empty(0)
     range2 = np.empty(0)
-    # range3 = np.empty(0)
+    range3 = np.empty(0)
+    range4 = np.empty(0)
+    range5 = np.empty(0)
+    range6 = np.empty(0)
 
-    current_file = open('complete_caprate.dat', 'r')
+    current_file = open('d8ACS.dat', 'r')
     lines = current_file.readlines()
 
     for x in lines:
         x = x.split('\t')
         domain = np.append(domain, float(x[0]))
-        range = np.append(range, float(x[1]))
-        # range2 = np.append(range2, float(x[2]))
-        # range3 = np.append(range3, float(x[3]))
+        range1 = np.append(range1, float(x[1]))
+        range2 = np.append(range2, float(x[2]))
+        range3 = np.append(range3, float(x[3]))
+        range4 = np.append(range4, float(x[4]))
+        range5 = np.append(range5, float(x[5]))
+        range6 = np.append(range6, float(x[6]))
 
     current_file.close
 
     fig, ax1 = plt.subplots()
-    ax1.loglog(domain, range, color='blue')
-    ax1.set(xlabel = r'$Radius$ [km]', ylabel = r'$C$')
+    ax1.loglog(domain, range1)
+    ax1.loglog(domain, range2)
+    ax1.loglog(domain, range3)
+    ax1.loglog(domain, range4)
+    ax1.loglog(domain, range5)
+    ax1.loglog(domain, range6)
+    ax1.set(xlabel = r'$m_\chi$ [GeV]', ylabel = r'$G^2$ $[GeV^{-2}]$')
     # ax1.set_yscale('log')
     # ax1.axis([0, 12, 0, 1.2e56])
-    plt.savefig('C.pdf')
-
-    # fig, ax2 = plt.subplots()
-    # ax2.plot(domain, range2, color='blue')
-    # ax2.set(xlabel = r'$Radius$ [km]', ylabel = r'$zeta n$')
-    # # ax2.set_yscale('log')
-    # # ax1.axis([0, 11.5, 0.4, 1])
-    # plt.savefig('zeta_times_n.png')
-
-    # fig, ax3 = plt.subplots()
-    # ax3.plot(domain, range3, color='blue')
-    # ax3.set(xlabel = r'$Radius$ [km]', ylabel = r'$C/V$')
-    # # ax3.set_yscale('log')
-    # # ax1.axis([0, 11.5, 0.4, 1])
-    # plt.savefig('no_density_cap.png')
+    plt.savefig('d8ACS.pdf')
 
 
-
-def full_plotter():
-
-    for name in files:
-        lines = []
-        domain = np.empty(0)
-        range = np.empty(0)
-
-        current_file = open(name + '.dat', 'r')
-        lines = current_file.readlines()
-
-        for x in lines:
-            x = x.split('\t')
-            domain = np.append(domain, float(x[0]))
-            range = np.append(range, float(x[1]) )
-
-        current_file.close
-
-        fig, ax1 = plt.subplots()
-        ax1.loglog(domain, range, color='blue')
-        ax1.set(xlabel = r'$m$ [GeV]', ylabel = r'$C$', title = name)
-        # ax1.set_yscale('log')
-        # ax1.axis([0, 12, 0, 1.2e56])
-        plt.savefig(name + '.png')
-
-# full_plotter()
-cap_rate_plots()
+ann_plots()
