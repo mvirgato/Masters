c008b4ae89cb1c1ce39ec4b3c7a2a64d30f950ad
mvirgato <michael.virgato@gmail.com>
2019-09-17 15:07:11 +1000

    ok

--- annihilation/plott_ann.py
+++ annihilation/plott_ann.py
@@ -1,12 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('science')
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 20
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 names = []
 
 for i in range(11)[1:]:
     names.append('d'+ str(i))
 
-def ann_plots():
+def ann_plots(figsize = (20, 11), dpi = 500):
 
     for j in names:
         lines = []
@@ -31,6 +45,6 @@ def ann_plots():
         ax1.set(xlabel = r'$m_\chi$ [GeV]', ylabel = r'$G^2$ $[GeV^{-2}]$')
         plt.savefig(j + 'ACS.pdf')
 
-    
+
 
 ann_plots()
